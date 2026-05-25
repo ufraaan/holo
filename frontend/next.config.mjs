a56116b4e7cbc28888fb86eca97e6de0f85094d0
ufraaan <ufraansiddiqui30@gmail.com>
2026-05-25 19:08:39 +0530

    feat: enable next-intl plugin in next.config
    Wrap the Next.js config with createNextIntlPlugin to enable server-
    side locale resolution per request. Chained before the Sentry wrapper
    so both plugins compose correctly.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 import { withSentryConfig } from '@sentry/nextjs';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
@@ -17,7 +21,7 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
+export default withSentryConfig(withNextIntl(nextConfig), {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
